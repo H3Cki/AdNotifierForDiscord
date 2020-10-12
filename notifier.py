e8f124abbc7449b1dae28c118e92695dac3ec2d5
H3Cki <H3Ckimail@users.noreply.github.com>
2020-10-12 15:16:49 +0200

    bug fixes

--- notifier.py
+++ notifier.py
@@ -7,6 +7,8 @@ from discord import Webhook, AsyncWebhookAdapter
 import aiohttp
 import asyncio
 import json
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 CONFIG = {}
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
@@ -147,9 +149,9 @@ def time_until(time,delta = timedelta()):
 	return future - now
 
 def start():
-	print(datetime.now(), "Notifier started")
 	first_run = True
 	load_config()
+	print(datetime.now(), "Notifier started")
 	start = CONFIG['notifier']['activity_hours']['start']
 	end = CONFIG['notifier']['activity_hours']['end']
 	while True:
