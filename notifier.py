fe597f4dee9a8b73d07e561768c457ef62e630bd
H3Cki <H3Ckimail@users.noreply.github.com>
2020-10-12 19:03:41 +0200

    Small QoL features

--- notifier.py
+++ notifier.py
@@ -122,14 +122,16 @@ async def handle_ads(new_ads):
 		else:
 			ads[href] = ad
 			to_notify.append(ad)
-
+	first_run = False
 	if first_run == False and len(to_notify):
-		await send(f'```css\n[{datetime.now().strftime("%H:%M")}] --- {len(to_notify)} new cars ---```')
+		await send(f'```css\n[{datetime.now().strftime("%H:%M")}] --- {len(to_notify)} new car{"s" if len(to_notify) != 1 else ""} ---```')
 		for i, ad in enumerate(to_notify):
 			await send(notification_text(ad,i))
-				
+	elif not len(to_notify):
+		print(datetime.now(), "No results")
 
 async def update():
+	print('update')
 	ads = []
 	for site in CONFIG['sites']:
 		if site['active']:
@@ -176,6 +178,7 @@ async def start():
 				st = time_until(sleep_until).total_seconds()
 			else:
 				st = random.uniform(_min,_max)
+				print(datetime.now(), f'Sleeping randomly for {int(st)} seconds.')
 		else:
 			st = time_until(CONFIG['notifier']['activity_hours']['start']).total_seconds()
 			print(datetime.now(), f'Sleeping until start ({start})')
